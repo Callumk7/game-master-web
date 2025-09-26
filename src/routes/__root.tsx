ae6bedd08017171dc2ea51cc31d92163ac05d994
Callum Kloos <callumkloos@gmail.com>
2025-09-26 19:12:43 +0100

    update swagger docs for prod

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,10 +3,8 @@ import type { QueryClient } from "@tanstack/react-query";
 import { createRootRouteWithContext, HeadContent, Scripts } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { createServerFn } from "@tanstack/react-start";
-import { useEffect } from "react";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "~/components/theme-provider";
-import { initializeApiClient } from "~/utils/api-client";
 import { getAppSession } from "~/utils/session";
 import TanStackQueryDevtools from "../integrations/tanstack-query/devtools";
 import appCss from "../styles.css?url";
@@ -62,10 +60,6 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 });
 
 function RootDocument({ children }: { children: React.ReactNode }) {
-	useEffect(() => {
-		initializeApiClient();
-	}, []);
-
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<head>
