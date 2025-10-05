5d4564cc236ff488c89493b4c5fc0659c53225ea
Callum Kloos <callumkloos@gmail.com>
2025-10-05 15:14:38 +0100

    increase size of popover window close button

--- src/components/draggable.tsx
+++ src/components/draggable.tsx
@@ -298,7 +298,7 @@ const DraggableWindow: React.FC<DraggableWindowProps> = ({
 						</Dialog.Title>
 						<Dialog.Close
 							className={cn(
-								"ring-offset-background focus:ring-ring data-[open]:bg-accent data-[open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+								"ring-offset-background focus:ring-ring data-[open]:bg-accent data-[open]:text-muted-foreground absolute top-2 right-2 p-2 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-5",
 							)}
 						>
 							<XIcon />
