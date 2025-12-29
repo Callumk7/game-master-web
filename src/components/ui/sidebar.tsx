f0a3e366725b35bf0c56ca7d3842b5a62c98d958
Callum Kloos <callum@beacon.co.uk>
2025-12-29 15:08:33 +0000

    scrollbars

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -276,7 +276,7 @@ const SidebarContent = ({ className, ref, ...props }: React.ComponentProps<"div"
 			ref={ref}
 			data-sidebar="content"
 			className={cn(
-				"flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden",
+				"flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden scrollbar-none",
 				className,
 			)}
 			{...props}
